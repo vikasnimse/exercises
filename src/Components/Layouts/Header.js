33f05f9f6257f1e3b19b31b55c4408fdae66e9ba
Vikas S Nimse <vikas.nimse@gmail.com>
2019-10-08 22:48:48 +0530

    changes

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
-import {AppBar, Toolbar, Typography} from "@material-ui/core";
-
-export default props =>
-    <AppBar position="static">
-        <Toolbar>
-        <Typography variant="h6" color="inherit">
-            Exercise
-        </Typography>
-        </Toolbar>
-    </AppBar>
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import Dialog from "../Exercises/Dialogs/Create";
+export default props => (
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="title" color="inherit" style={{ flex: 1 }}>
+        Exercises
+      </Typography>
+      <Dialog />
+    </Toolbar>
+  </AppBar>
+);
